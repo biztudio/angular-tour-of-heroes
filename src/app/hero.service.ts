860469fbb65b697f5c904f48977c4fd23ee5f469
Yang SHEN <bizbetter@outlook.com>
2020-06-09 23:17:17 +0800

    use service load data async.

---
+++ src/app/hero.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+import { Observable, of } from 'rxjs';
+
+
+// @Injectable() 装饰器。 它把这个类标记为依赖注入系统的参与者之一。
+// 在 @Injectable 元数据中注册该提供者，还能允许 Angular 通过移除那些完全没有用过的服务来进行优化。
+@Injectable({
+  // 必须先注册一个服务提供者，来让 HeroService 在依赖注入系统中可用.
+  // 所谓服务提供者就是某种可用来创建或交付一个服务的东西；
+  // 在这里，它通过实例化 HeroService 类，来提供该服务。
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+
+  /*
+    现实应用重的服务返回结果由于要等待服务端的 response, 所以，通常是异步的。
+    这种场景可以通过返回 Observable 来实现 ( 比如， HttpClient.get() 会返回 Observable )
+    Observable 是 RxJS 库中的一个关键类
+  */
+  getHeroesAsync(): Observable<Hero[]> {
+    // of(HEROES) 会返回一个 Observable<Hero[]>，
+    // 它会发出单个值，这个值就是这些模拟英雄的数组。
+    // 如果从真实的服务器请求，HttpClient.get<Hero[]>() 也同样返回一个 Observable<Hero[]>
+    return of(HEROES);
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service';
+/* import { HEROES } from '../mock-heroes'; */
 
 @Component({
   // CSS 元素选择器 app-heroes 用来在"父组件"的模板中匹配 HTML 元素的名称，以识别出该组件。
@@ -14,12 +16,26 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  constructor() { }
+  // 1. 声明了一个私有 heroService 属性，
+  // 2. 把它标记为一个 HeroService 的注入点。
+  // 当 Angular 创建 HeroesComponent 时，依赖注入系统就会把这个 heroService 参数设置为 HeroService 的"单例对象"。
+  // 让构造函数保持简单，只做初始化操作，比如把构造函数的参数赋值给属性。
+  // 构造函数不应该做任何事。 它当然不应该调用某个函数来向远端服务（比如真实的数据服务）发起 HTTP 请求。
+  constructor(private heroService: HeroService) { }
 
   // 一个生命周期钩子，Angular 在创建完组件后很快就会调用 ngOnInit()。
   // 这里是放置初始化逻辑的好地方。
   ngOnInit(): void {
-    this.heroes = HEROES;
+    /*
+    // HeroService.getHeroes() 的函数签名是同步的
+    this.heroes = this.heroService.getHeroes();
+     */
+
+     // 通过订阅 ( subscribe ) 异步操作
+    this.heroService.getHeroesAsync().subscribe(heroes => {
+      this.heroes = heroes;
+      console.log('fetched.');
+    });
   }
 
   onSelect(hero: Hero): void {
