808a0b918eea93093c670c29282b1ef068e24e73
Yang SHEN <bizbetter@outlook.com>
2020-06-10 17:43:10 +0800

    consume the demo webapi in memory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessageComponent } from './message/message.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+
+// 模拟服务端数据
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -17,6 +22,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
+
     BrowserModule,
     FormsModule,
     AppRoutingModule
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
+
+// 使用 RxJS 的 catchError() 操作符来建立对 Observable 结果的处理管道（pipe）
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 // @Injectable() 装饰器。 它把这个类标记为依赖注入系统的参与者之一。
@@ -15,27 +18,72 @@ import { Observable, of } from 'rxjs';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
   /*
     现实应用重的服务返回结果由于要等待服务端的 response, 所以，通常是异步的。
     这种场景可以通过返回 Observable 来实现 ( 比如， HttpClient.get() 会返回 Observable )
     Observable 是 RxJS 库中的一个关键类
+    本方法的调用者调用该方法的后面接一个订阅 .subscribe 来在数据返回后进一步处理.
   */
   getHeroesAsync(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     // of(HEROES) 会返回一个 Observable<Hero[]>，
     // 它会发出单个值，这个值就是这些模拟英雄的数组。
     // 如果从真实的服务器请求，HttpClient.get<Hero[]>() 也同样返回一个 Observable<Hero[]>
-    return of(HEROES);
+    // return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+    // 建立对 Observable 结果的处理管道（pipe）
+    .pipe(
+      // 管道中的 catchError() 操作符会拦截失败的 Observable。 它把错误对象传给错误处理器，错误处理器会处理这个错误。
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
+  // 本方法的调用者调用该方法的后面接一个订阅 .subscribe 来在数据返回后进一步处理.
   getHeroAsync(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(h => h.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    //  return of(HEROES.find(h => h.id === id));
+
+    // 建立对 Observable 结果的处理管道（pipe）
+    return this.http.get<Hero>(url).pipe(
+
+      // 管道中  tap() 操作符可以查看 Observable 中的值，使用那些值做一些事情，并且把它们传出来。
+      // 这种 tap() 回调不会改变这些值本身。
+      tap(_ => this.log(`fetched hero id=${id}`)),
+
+      // 管道中的 catchError() 操作符会拦截失败的 Observable。 它把错误对象传给错误处理器，错误处理器会处理这个错误。
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
